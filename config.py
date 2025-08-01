76e39a0e505bb85f8e788b1e35766ee9898e20de
Ashish <ashishdevil117@gmail.com>
2025-08-01 03:48:11 -0400

    database configuration file

---
+++ config.py
@@ -0,0 +1,22 @@
+# config.py
+
+# --- DATABASE CONFIGURATION ---
+hostname = ""
+database= ""
+port = ""
+username = ""
+password= ""
+DB_CONFIG = {
+    "host": hostname,       # Replace with your hostname
+    "database": database,   # Replace with your database name
+    "port": port,           # Replace with your port (e.g., "3306")
+    "user": username,                 # Replace with your username
+    "password": password    # Replace with your password
+}
+
+# --- LLM CONFIGURATION ---
+LLM_MODEL_NAME = "deepseek-r1:1.5b"
+
+
+
+
